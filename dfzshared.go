f58111a16b0a7c41467bc58ee742da4f2f8e4c69
Rob Hekkelman <robhekkelman@MacBook-Pro-van-Rob.local>
2017-05-19 10:29:00 +0200

    .

--- dfzshared.go
+++ dfzshared.go
@@ -156,7 +156,7 @@ type ContractedTreatment struct {
 
 type HealthCareContractRouter struct {
 	UzoviCode string `json:"UzoviCode"`
-	RESTUrl   string `json:"RESTSUrl"`
+	RESTUrl   string `json:"RESTUrl"`
 }
 
 type WalletTransaction struct {
